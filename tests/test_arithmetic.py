1fb68336f8d75cad79fd121a541fa428d07dd390
Alex Dunn <dunn.alex@icloud.com>
2015-05-07 11:55:59 -0700

    Remove a temporary unit test
    This was not intended to be pushed up stream.

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -10,16 +10,3 @@ def test_multiplication_game():
     assert arithmetic.multiplication_game(17) == False
     assert arithmetic.multiplication_game(34012226) == True
 
-    expected = True
-    input_value = 0
-    with open("/Users/admin/Desktop/outputs.txt") as file:
-        for line in file:
-            input_value += 1
-            if line.replace('\n', '') == "Stan wins.":
-                print("Stan expected to win")
-                expected = True
-            else:
-                print("Ollie expected to win")
-                expected = False
-            print("input_value = {}" .format(str(input_value)))
-            assert arithmetic.multiplication_game(input_value) == expected
